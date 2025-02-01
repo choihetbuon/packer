b6e5580351e8487a92c5f34b73bf3b5b20a9bd25
Brian Vo <datvo2k@gmail.com>
2025-02-01 20:44:05 +0700

    add Ansible playbooks for base configuration and cleanup; update .gitignore

--- scripts/change_repo.sh
+++ scripts/change_repo.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
 sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo
 sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/*.repo
 
+
+yum clean all
 yum update -y
 yum upgrade -y
